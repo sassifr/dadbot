185de346de2a5178e1017c32b43e65af9dae5793
sassifr <alladai880@gmail.com>
2023-02-06 21:19:22 -0500

    token not work

---
+++ bot.py
@@ -0,0 +1,35 @@
+import discord
+import re
+import os
+
+def run_disc_bot():
+    tkn = os.getenv('DISCORD_TOKEN')
+
+    print(tkn)
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents = intents)
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        thing = re.findall("[iI]m\s\s*\w\w*",message.content)
+        thing2 = re.findall("[iI]'m\s\s*\w\w*",message.content)
+        if thing:
+            name = re.findall("\s\s*\w\w*", thing[0])[0]
+            to_send = "hi" + name + ", im dadBot"
+            await message.channel.send(to_send)
+        elif thing2:
+            name = re.findall("\s\s*\w\w*", thing[0])[0]
+            to_send = "hi" + name + ", i'm dadBot"
+            await message.channel.send(to_send)
+        elif client.user == "gates#9081":
+            await message.channel.send("my regex is better than yours")
+
+    client.run(tkn)
+    
+
+    
+
--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-import discord
+import bot
 import os
 
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run(os.getenv('TOKEN'))
+if __name__ == '__main__':
+    # run bot
+    bot.run_disc_bot()
+    pass
